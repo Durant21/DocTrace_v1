a1b607fa70a7f3b7740ce0a2ed988e069251d5e6
Dante <durant.crimson@icloud.com>
2019-06-24 17:34:27 -0600

    initial commit

---
+++ DocTrace/doctrace/__init__.py
@@ -0,0 +1,24 @@
+from pyramid.config import Configurator
+from doctrace.data.db_factory import DbSessionFactory
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.scan()
+
+    with Configurator(settings=settings) as config:
+        init_db(config)
+
+    return config.make_wsgi_app()
+
+
+def init_db(config):
+    settings = config.get_settings()
+    db_file = settings.get('db_filename')
+
+    DbSessionFactory.global_init(db_file)
