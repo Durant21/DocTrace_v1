5d4a6895812fdbe6779ad26ffbcbc853f1ef05f5
dante <durant.crimson@icloud.com>
2019-07-09 13:15:31 -0600

    add classes

--- DocTrace/doctrace/views.py
+++ DocTrace/doctrace/views.py
@@ -1,9 +1,14 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer="templates/mytemplate.jinja2")
 def my_view(request):
     return {'project': 'DocTrace'}
+
+# @view_config(route_name='home', renderer="templates/mytemplate.pt")
+# def my_view(request):
+#     return {'project': 'iMii_v9'}
+
 
 @view_config(route_name='main', renderer='templates/main_template.jinja2')
 def main_view1(request):
